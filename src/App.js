1a4e45abc90a7d330b2d96c164b0422af4325157
younis3 <ahmady00003@gmail.com>
2021-12-10 06:11:38 +0200

    dev 2nd commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h2>Task Tracker App</h2>
+        <h1>Task Tracker App</h1>
       </header>
     </div>
   );
