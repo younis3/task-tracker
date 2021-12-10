cb9aa11e0861479e9e70280967ae208c271b33a6
younis3 <ahmady00003@gmail.com>
2021-12-11 01:02:02 +0200

    task component html and style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { useState } from "react";
 import Form from "./components/Form";
@@ -5,15 +6,20 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [input, setInput] = useState("");
-  const [toDoList, setToDoList] = useState([{}]);
+  const [toDoList, setToDoList] = useState([]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Task Tracker App</h1>
       </header>
-      <Form input={input} setInput={setInput} toDoList={toDoList} setToDoList={setToDoList} />
-      <TodoList key={toDoList.id} toDoList={toDoList} />
+      <Form
+        input={input}
+        setInput={setInput}
+        toDoList={toDoList}
+        setToDoList={setToDoList}
+      />
+      <TodoList toDoList={toDoList} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import TodoItem from "./TodoItem";
+import styles from "../styles/global.module.css";
 
-const TodoList = ({toDoList}) => {
-    return (
-        <div>
-            {toDoList.map(item => (
-                <h1>{item.text}</h1>
-            ))}
-        </div>
-    )
-}
+const TodoList = ({ toDoList }) => {
+  return (
+    <div className={styles["toDoListContainer"]}>
+      <ul className={styles["toDoList"]}>
+        {toDoList.map((item) => (
+          <TodoItem
+            key={item.id}
+            text={item.text}
+            completed={item.completed}
+            id={item.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
