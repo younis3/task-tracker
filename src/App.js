1e39654fe1fe1997e4ac268bc9f04c7ca728d63f
younis3 <ahmady00003@gmail.com>
2021-12-11 04:51:43 +0200

    toggle between completed and not completed is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [input, setInput] = useState("");
   const [toDoList, setToDoList] = useState([]);
+  const [slct, setSlct] = useState("all");
+  const [filteredList, setFilteredList] = useState([]);
+
+  useEffect(() => {
+    switch (slct.toLowerCase()) {
+      case "completed":
+        setFilteredList(toDoList.filter((el) => el.completed === true));
+        break;
+      case "not completed":
+        setFilteredList(toDoList.filter((el) => el.completed === false));
+        break;
+      default:
+        setFilteredList(toDoList);
+    }
+  }, [toDoList, slct]);
 
   return (
     <div className="App">
@@ -18,8 +33,13 @@ function App() {
         setInput={setInput}
         toDoList={toDoList}
         setToDoList={setToDoList}
+        setSlct={setSlct}
       />
-      <TodoList toDoList={toDoList} setToDoList={setToDoList} />
+      <TodoList
+        toDoList={toDoList}
+        setToDoList={setToDoList}
+        filteredList={filteredList}
+      />
     </div>
   );
 }
